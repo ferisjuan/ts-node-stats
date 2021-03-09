1055b74828dd14d6a42b68ba992b566c13003079
Juan Feris <juan.feris@gorillalogic.com>
2021-03-09 14:08:35 -0500

    Data Parser

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -4,8 +4,10 @@ import { MatchResult } from './MatchResult'
 
 import { dateStringToDate } from './utils'
 
+type MatchData = [Date, string, string, number, number, MatchResult, string]
+
 export class CsvFileReader {
-	data: string[][] = []
+	data: MatchData[] = []
 
 	constructor(public filename: string) {}
 
@@ -16,14 +18,16 @@ export class CsvFileReader {
 			})
 			.split('\n')
 			.map((row: string): string[] => row.split(','))
-			.map((row: string[]): any => [
-				dateStringToDate(row[0]),
-				row[1],
-				row[2],
-				parseInt(row[3]),
-				parseInt(row[4]),
-				row[5] as MatchResult,
-				row[6],
-			])
+			.map(
+				(row: string[]): MatchData => [
+					dateStringToDate(row[0]),
+					row[1],
+					row[2],
+					parseInt(row[3]),
+					parseInt(row[4]),
+					row[5] as MatchResult,
+					row[6],
+				]
+			)
 	}
 }
